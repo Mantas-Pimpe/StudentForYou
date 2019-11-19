f2e9a04b8c2d5b6a4359e2793bc7f3d94896fa10
Mantas Pimpe <mantas.pimpe@gmail.com>
2019-11-19 16:43:53 +0200

    npm

--- StudentForYou/StudentForYou.WebApp/ClientApp/src/layouts/Auth.jsx
+++ StudentForYou/StudentForYou.WebApp/ClientApp/src/layouts/Auth.jsx
@@ -44,8 +44,7 @@ class Auth extends React.Component {
                   <Col lg="5" md="6">
                     <h1 className="text-white">Welcome!</h1>
                     <p className="text-lead text-light">
-                      Use these awesome forms to login or create new account in
-                      your project for free.
+                      Login into the Student For You system!
                     </p>
                   </Col>
                 </Row>
