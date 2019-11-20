c2addddf70d80994196abe4f529aedbc0bc9ce39
Karolis Smaznovas <Ksmaznovas@gmail.com>
2019-11-20 19:53:54 +0200

    QuestionController

---
+++ StudentForYou/StudentForYou.WebApp/Controllers/QuestionController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using StudentForYou.WebApp.Models;
+
+namespace StudentForYou.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : DataBaseController
+    {
+        // GET: api/Question
+        [HttpGet]
+        public List<Question> Get()
+        {
+            List<Question> questionList = new List<Question>();
+            using (MySqlConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                con.Open();
+                var qry = "select qns.qns_id, qns.qns_likes, qns.qns_views, qns.qns_comments, qns.qns_text, qns.qns_name, qns.qns_creation_date from questions qns";
+                using (MySqlCommand cmd = new MySqlCommand(qry, con))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            questionList.Add(new Question(reader.GetInt32(0), reader.GetInt32(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetString(4), reader.GetString(5), reader.GetDateTime(6)));
+                        }
+                    }
+                }
+                con.Close();
+            }
+            return questionList;
+        }
+
+        // GET: api/Question/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Question
+        [HttpPost]
+        public void Post([FromBody] Question question)
+        {
+            var qry = "INSERT INTO questions(qns_name, qns_text, qns_creation_date, qns_user_id, qns_views, qns_likes, qns_comments) VALUES (@qns_name, @qns_text, @qns_creation_date, @qns_user_id, @qns_views, @qns_likes, @qns_comments)";
+            using (MySqlConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(qry, con))
+                {
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@qns_name", qns_name.Trim());
+                    cmd.Parameters.AddWithValue("@qns_text", qns_text.Trim());
+                    cmd.Parameters.AddWithValue("@qns_creation_date", qns_creation_date);
+                    cmd.Parameters.AddWithValue("@qns_user_id", qns_user_id);
+                    cmd.Parameters.AddWithValue("@qns_views", qns_views);
+                    cmd.Parameters.AddWithValue("@qns_likes", qns_likes);
+                    cmd.Parameters.AddWithValue("@qns_comments", qns_comments);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+
+        // PUT: api/Question/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StudentForYou/StudentForYou.WebApp/Models/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentForYou.WebApp.Models
+{
+    public class Question
+    {
+        public int questionID;
+        public int questionLikes;
+        public int questionViews;
+        public int questionAnswers;
+        public string questionText;
+        public string questionName;
+        public DateTime questionCreationDate;
+
+        public Question(int questionID, int questionLikes, int questionViews, int questionAnswers, string questionText, string questionName, DateTime questionCreationDate)
+        {
+            this.questionID = questionID;
+            this.questionLikes = questionLikes;
+            this.questionViews = questionViews;
+            this.questionAnswers = questionAnswers;
+            this.questionText = questionText;
+            this.questionName = questionName;
+            this.questionName = questionName;
+            this.questionCreationDate = questionCreationDate;
+        }
+    }
+}
