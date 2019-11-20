7afe795626fd5c892f4e5803a72c610470031d2f
Mantas Pimpe <mantas.pimpe@gmail.com>
2019-11-20 14:59:50 +0200

    dd

---
+++ StudentForYou/StudentForYou.WebApp/ClientApp/src/views/examples/CoursesChat.jsx
@@ -0,0 +1,28 @@
+﻿
+import React from "react";
+import Icons from "./Icons.jsx";
+// reactstrap components
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    Container,
+    Row,
+    Col
+} from "reactstrap";
+// core components
+import Header from "../../components/Headers/Header.jsx";
+class CoursesChat extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default CoursesChat;
