988cb074bc8fe193def589590f1e007428c30988
Mantas_P <54863970+Mantas-Pimpe@users.noreply.github.com>
2019-11-22 03:24:35 +0200

    fix redirect

--- StudentForYou/StudentForYou.WebApp/ClientApp/src/index.js
+++ StudentForYou/StudentForYou.WebApp/ClientApp/src/index.js
@@ -15,6 +15,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
