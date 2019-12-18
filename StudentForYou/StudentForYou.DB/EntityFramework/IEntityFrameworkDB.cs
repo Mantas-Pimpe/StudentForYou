33fe61d86b008b64b5099600793edda72186651e
Mantas Pimpe <mantas.pimpe@gmail.com>
2019-12-18 13:18:21 +0200

    Added Entity Framework

---
+++ StudentForYou/StudentForYou.DB/EntityFramework/IEntityFrameworkDB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+/*
+ * This file would be used if we converted to use the Entity Framework
+ */
+namespace StudentForYou.DB
+{
+    public interface IEntityFrameworkDB<T> where T : class
+    {
+        IEnumerable<T> GetModelList();
+
+        T GetModelByID(int id);
+
+        void InsertModel(T entity);
+
+        void UpdateModel(T entity);
+
+        void DeleteModel(int id);
+
+        void Save();
+    }
+}
